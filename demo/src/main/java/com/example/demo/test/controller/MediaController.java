fa967bcaa82dd9b5105633eb683cc93037ef5d86
Davron-Adbusalomov <2110117@newuzbekistanuniversity.uz>
2024-04-08 08:12:10 +0500

    question image updated

--- demo/src/main/java/com/example/demo/test/controller/MediaController.java
+++ demo/src/main/java/com/example/demo/test/controller/MediaController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,7 +19,7 @@ public class MediaController {
     private MediaService mediaService;
 
     @PostMapping("/upload")
-    public ResponseEntity<String> uploadMedia(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<String> uploadMedia(@RequestBody MultipartFile file) {
         try {
             String url = mediaService.uploadMedia(file);
             return ResponseEntity.ok(url);
