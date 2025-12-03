1a71af19df0ad348314a4577feb80bc3d6811b35
DAVRON <abdusalomovdavron3703@gmail.com>
2025-12-03 16:51:27 +0500

    Add SpringDoc OpenAPI dependency and permit access to Swagger UI endpoints

--- demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -37,28 +37,28 @@
 //    private final UserPermissionsRepository userPermissionsRepository;
 //
 //
-////    @Bean
-////    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-////        http.cors(Customizer.withDefaults());
-////        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests((authorize) -> {
-////            authorize.requestMatchers(HttpMethod.GET,"/actuator/**").permitAll();
-////            authorize.requestMatchers(HttpMethod.POST, "/api/v1/authentication/sign-in", "/api/v1/authentication/refresh-token").permitAll();
-////            authorize.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
-////            authorize.requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll();
-////            authorize.requestMatchers(HttpMethod.GET, "/api-docs/**").permitAll();
-////            authorize.anyRequest().authenticated();
-////        });
-////
-////        http.sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-////
-////        http.exceptionHandling(exception -> exception.authenticationEntryPoint(authenticationEntryPoint));
-////
-////        http.userDetailsService(userDetailsService);
-////        http.authenticationProvider(authenticationProvider);
-////        http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
-////
-////        return http.build();
-////    }
+/// /    @Bean
+/// /    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+/// /        http.cors(Customizer.withDefaults());
+/// /        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests((authorize) -> {
+/// /            authorize.requestMatchers(HttpMethod.GET,"/actuator/**").permitAll();
+/// /            authorize.requestMatchers(HttpMethod.POST, "/api/v1/authentication/sign-in", "/api/v1/authentication/refresh-token").permitAll();
+/// /            authorize.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
+/// /            authorize.requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll();
+/// /            authorize.requestMatchers(HttpMethod.GET, "/api-docs/**").permitAll();
+/// /            authorize.anyRequest().authenticated();
+/// /        });
+/// /
+/// /        http.sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+/// /
+/// /        http.exceptionHandling(exception -> exception.authenticationEntryPoint(authenticationEntryPoint));
+/// /
+/// /        http.userDetailsService(userDetailsService);
+/// /        http.authenticationProvider(authenticationProvider);
+/// /        http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+/// /
+/// /        return http.build();
+/// /    }
 //
 //    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 //
@@ -132,7 +132,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -148,12 +147,12 @@ public class SecurityConfig {
 
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for stateless APIs
                 .cors(Customizer.withDefaults()) // Apply the CORS configuration defined below
 
-                .authorizeHttpRequests((request)->request
+                .authorizeHttpRequests((request) -> request
                         // Explicitly permit OPTIONS requests for CORS preflight checks
                         .requestMatchers(HttpMethod.POST, "/**").permitAll()
                         // Permit specific authentication endpoints
@@ -168,7 +167,7 @@ public class SecurityConfig {
                         .anyRequest()
                         .authenticated())
 
-                .sessionManagement((session)->session
+                .sessionManagement((session) -> session
                         // Use stateless sessions, typical for JWT-based authentication
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
@@ -195,7 +194,6 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", config); // apply to all paths
         return source;
     }
-
 
 
     @Bean
