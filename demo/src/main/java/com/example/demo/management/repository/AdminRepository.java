faa07b8cd4de1cde9e1b29debcf76f7ebc03b8f5
DAVRON <abdusalomovdavron3703@gmail.com>
2025-12-15 23:45:11 +0500

    Remove unused methods from AdminRepository to streamline codebase

--- demo/src/main/java/com/example/demo/management/repository/AdminRepository.java
+++ demo/src/main/java/com/example/demo/management/repository/AdminRepository.java
@@ -8,7 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
-    Optional<Admin> findByUsernameAndPassword(String username, String password);
 
-    Optional<Admin> findByUsername(String username);
 }
