c4ceabb0f960bd4f73908b93dc0f6125276ca3ad
DAVRON <abdusalomovdavron3703@gmail.com>
2025-12-18 00:15:28 +0500

    Add Subject management functionality; implement SubjectController, DTO, Service, Mapper, and Repository with CRUD operations and permissions

--- demo/src/main/java/com/example/demo/management/service/SubjectService.java
+++ demo/src/main/java/com/example/demo/management/service/SubjectService.java
@@ -12,7 +12,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.UUID;
 
 @Service
