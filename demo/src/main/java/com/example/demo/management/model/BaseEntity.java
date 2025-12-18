69eed8f71165afce0043d3c513add0eabaf38af7
DAVRON <abdusalomovdavron3703@gmail.com>
2025-12-19 00:06:42 +0500

    Refactor group and attendance management; update DTOs to use UUIDs, modify endpoint paths for consistency, and enhance service methods for better data handling

--- demo/src/main/java/com/example/demo/management/model/BaseEntity.java
+++ demo/src/main/java/com/example/demo/management/model/BaseEntity.java
@@ -1,81 +1,80 @@
-    package com.example.demo.management.model;
+package com.example.demo.management.model;
 
-    import com.example.demo.config.CurrentUserUtils;
-    import com.example.demo.enums.Status;
-    import jakarta.persistence.*;
-    import lombok.Getter;
-    import lombok.Setter;
-    import org.hibernate.annotations.FilterDef;
-    import org.hibernate.annotations.ParamDef;
-    import org.springframework.data.annotation.CreatedBy;
-    import org.springframework.data.annotation.CreatedDate;
-    import org.springframework.data.annotation.LastModifiedBy;
-    import org.springframework.data.annotation.LastModifiedDate;
-    import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-    import org.springframework.security.core.userdetails.UserDetails;
+import com.example.demo.config.CurrentUserUtils;
+import com.example.demo.enums.Status;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.ParamDef;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-    import java.time.LocalDateTime;
+import java.time.LocalDateTime;
 
-    @Getter
-    @Setter
-    @MappedSuperclass
-    @EntityListeners(AuditingEntityListener.class)
-    @FilterDef(
-            name = "centerFilter",
-            parameters = @ParamDef(name = "centerId", type = java.util.UUID.class)
-    )
-    public abstract class BaseEntity {
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@FilterDef(
+        name = "centerFilter",
+        parameters = @ParamDef(name = "centerId", type = java.util.UUID.class)
+)
+public abstract class BaseEntity {
 
-        @CreatedDate
-        @Column(name = "created_at", nullable = false, updatable = false)
-        private LocalDateTime createdAt;
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 
-        @LastModifiedDate
-        @Column(name = "updated_at")
-        private LocalDateTime updatedAt;
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
-        @Column(name = "status", nullable = false)
-        @Enumerated(EnumType.STRING)
-        private  Status status = Status.CREATED;
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.CREATED;
 
-        @CreatedBy
-        @Column(name = "created_by", updatable = false)
-        private Long createdBy;
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private Long createdBy;
 
-        @LastModifiedBy
-        @Column(name = "updated_by")
-        private Long updatedBy;
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private Long updatedBy;
 
-        @PrePersist
-        @PreUpdate
-        protected void onCreate() {
-            createdAt = LocalDateTime.now();
-            createdBy = CurrentUserUtils.getUserId();
-            status = Status.CREATED;
+    @PrePersist
+    @PreUpdate
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        createdBy = CurrentUserUtils.getUserId();
+        status = Status.CREATED;
 
-            handleDerivedFields();
-        }
-
-        public void updateEntity() {
-            if (this.updatedAt == null) {
-                this.updatedAt = LocalDateTime.now();
-            }
-            if (this.updatedBy == null) {
-                this.updatedBy = 1L;
-            }
-            if (this.status == null) {
-                this.status = Status.CREATED;
-            }
-
-            if (this.createdAt == null) {
-                this.createdAt = LocalDateTime.now();
-            }
-            if (this.createdBy == null) {
-                this.createdBy = 1L;
-            }
-        }
-
-        public void handleDerivedFields() {
-        }
-
+        handleDerivedFields();
     }
+
+    public void updateEntity() {
+        if (this.updatedAt == null) {
+            this.updatedAt = LocalDateTime.now();
+        }
+        if (this.updatedBy == null) {
+            this.updatedBy = 1L;
+        }
+        if (this.status == null) {
+            this.status = Status.CREATED;
+        }
+
+        if (this.createdAt == null) {
+            this.createdAt = LocalDateTime.now();
+        }
+        if (this.createdBy == null) {
+            this.createdBy = 1L;
+        }
+    }
+
+    public void handleDerivedFields() {
+    }
+
+}
