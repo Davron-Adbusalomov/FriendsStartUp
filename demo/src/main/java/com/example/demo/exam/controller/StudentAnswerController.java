87f39fd92f837005a29360d4f5c85d3f9f480b70
DAVRON <abdusalomovdavron3703@gmail.com>
2026-01-17 17:35:40 +0500

    Add functionality to retrieve written answers; implement StudentWrittenAnswersDTO and update StudentAnswerService and StudentAnswerController

--- demo/src/main/java/com/example/demo/exam/controller/StudentAnswerController.java
+++ demo/src/main/java/com/example/demo/exam/controller/StudentAnswerController.java
@@ -7,15 +7,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
-@RestController("/api/v1/student-answer")
+@RestController
+@RequestMapping("api/v1/student-answer")
 @AllArgsConstructor
 public class StudentAnswerController {
 
