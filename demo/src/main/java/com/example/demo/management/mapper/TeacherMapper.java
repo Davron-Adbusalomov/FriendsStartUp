066baced42b0ade6531958142f56fa837711a9b1
Davron-Adbusalomov <2110117@newuzbekistanuniversity.uz>
2026-05-06 22:47:43 +0500

    Refactor teacher update functionality to use UpdateTeacherRequest DTO and adjust image handling

--- demo/src/main/java/com/example/demo/management/mapper/TeacherMapper.java
+++ demo/src/main/java/com/example/demo/management/mapper/TeacherMapper.java
@@ -53,7 +53,6 @@ public interface TeacherMapper {
     }
 
     @Mapping(target = "subject", expression = "java(getSubjectDto(teacher))")
-    @Mapping(target = "imageUrl", source = "image")
     @Mapping(target = "image", ignore = true)
     TeacherInfoDTO toInfoDto(Teacher teacher);
 
