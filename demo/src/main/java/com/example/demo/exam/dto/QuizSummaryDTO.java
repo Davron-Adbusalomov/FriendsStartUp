bcf0182ab9001e9fe8aa2bb449e67a471a7a22d1
DAVRON <abdusalomovdavron3703@gmail.com>
2026-06-03 19:47:21 +0500

    Refactor mark handling in Question and StudentAnswerService to support null values

--- demo/src/main/java/com/example/demo/exam/dto/QuizSummaryDTO.java
+++ demo/src/main/java/com/example/demo/exam/dto/QuizSummaryDTO.java
@@ -1,6 +1,7 @@
 package com.example.demo.exam.dto;
 
 import com.example.demo.enums.QuizContentStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -12,6 +13,7 @@ public class QuizSummaryDTO {
     private String title;
     private Long duration;
     private int questionsNum;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime startTime;
     private QuizContentStatus status;
 }
