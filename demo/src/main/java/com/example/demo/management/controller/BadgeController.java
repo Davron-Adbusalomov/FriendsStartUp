2fbdfd5e42c36a81809edc828d113fbce24bed4f
DAVRON <abdusalomovdavron3703@gmail.com>
2026-06-04 19:31:48 +0500

    Add group ID to quiz results and update performance query

--- demo/src/main/java/com/example/demo/management/controller/BadgeController.java
+++ demo/src/main/java/com/example/demo/management/controller/BadgeController.java
@@ -15,7 +15,7 @@ import java.util.Locale;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/badges")
+@RequestMapping("/api/v1/badges")
 @RequiredArgsConstructor
 public class BadgeController {
 
