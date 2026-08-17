0323f302b625c6839f4178f16d105a9b1214b3f6
DAVRON <abdusalomovdavron3703@gmail.com>
2026-08-17 16:32:57 +0500

    feat: enhance YouTube OAuth integration with access token management and authorization flow

--- demo/src/main/java/com/example/demo/youtube/YoutubeController.java
+++ demo/src/main/java/com/example/demo/youtube/YoutubeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/youtube")
+@RequestMapping("/api/v1/youtube")
 @RequiredArgsConstructor
 public class YoutubeController {
 
