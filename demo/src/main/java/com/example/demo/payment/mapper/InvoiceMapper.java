966ee04a40ed40caf619ea9732316950dfe065dd
DAVRON <abdusalomovdavron3703@gmail.com>
2026-08-25 18:01:11 +0500

    feat: add user information to student balance response and invoice models

--- demo/src/main/java/com/example/demo/payment/mapper/InvoiceMapper.java
+++ demo/src/main/java/com/example/demo/payment/mapper/InvoiceMapper.java
@@ -8,7 +8,7 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface InvoiceMapper {
 
-    @Mapping(target = "userFullName", ignore = true)
+    @Mapping(target = "userFullName", expression = "java(invoice.getUser() != null ? invoice.getUser().getFullName() : null)")
     @Mapping(target = "groupName", expression = "java(invoice.getGrouping() != null ? invoice.getGrouping().getName() : null)")
     @Mapping(target = "paidAmount", ignore = true)
     InvoiceDTO toDto(Invoice invoice);
